0cdc75a6c4c86865ef761773477f8c05da7dc600
jorgealberto23 <jorge.rocha5@etec.sp.gov.br>
2024-09-27 22:55:39 -0300

    estilizacao

--- componentes/Home.js
+++ componentes/Home.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert } from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert, ImageBackground } from "react-native";
 import { firestore } from "../firebase";
 import { collection, onSnapshot, deleteDoc, doc } from "firebase/firestore";
 
-export default function Home({ navigation }) {
+const image = require('../assets/fundo.jpg'); // Ajuste o caminho da imagem conforme necessário
 
+export default function Home({ navigation }) {
     const [musicas, setMusicas] = useState([]);
 
     async function deleteMusicas(id) {
         try {
-            await deleteDoc(doc(firestore, "tbmusica", id))
-            Alert.alert("A musica foi deletada.")
+            await deleteDoc(doc(firestore, "tbmusica", id));
+            Alert.alert("A música foi deletada.");
         } catch (error) {
-            console.error("Erro ao deletar.", error)
+            console.error("Erro ao deletar.", error);
         }
     }
 
@@ -26,41 +27,63 @@ export default function Home({ navigation }) {
         });
         return () => unsubcribe();
     }, []);
-    
+
     return (
-        <View>
-            <View>
-                <Text>Lista das Musicas</Text>
-            </View>
-            <FlatList
-                data={musicas}
-                renderItem={({ item }) => {
-                    return (
-                        <View>
-                            <TouchableOpacity onPress={() => navigation.navigate("Alterar", {
-                                id: item.id,
-                                nomeMusica: item.nomeMusica,
-                                artistaMusica: item.artistaMusica,
-                                albumMusica: item.albumMusica
-                            })}>
-                                <View>
-                                    <Text> Nome da Musica: <Text>{item.nomeMusica}</Text></Text>
-                                    <Text> Nome do Artista: <Text>{item.artistaMusica}</Text></Text>
-                                    <Text> Nome do Album <Text>{item.albumMusica}</Text></Text>
-                                </View>
-                            </TouchableOpacity>
+        <ImageBackground source={image} style={styles.background}>
+            <View style={styles.container}>
+                <View>
+                    <Text>Lista das Músicas</Text>
+                </View>
+                <FlatList
+                    data={musicas}
+                    renderItem={({ item }) => {
+                        return (
                             <View>
-                                <TouchableOpacity onPress={() => { deleteMusicas(item.id) }}>
-                                    <Text>X</Text>
+                                <TouchableOpacity onPress={() => navigation.navigate("Alterar", {
+                                    id: item.id,
+                                    nomeMusica: item.nomeMusica,
+                                    artistaMusica: item.artistaMusica,
+                                    albumMusica: item.albumMusica
+                                })}>
+                                    <View style={styles.boxInfo}>
+                                        <Text style={styles.text}>Nome da Música: <Text style={styles.text}>{item.nomeMusica}</Text></Text>
+                                        <Text style={styles.text}>Nome do Artista: <Text style={styles.text}>{item.artistaMusica}</Text></Text>
+                                        <Text style={styles.text}>Nome do Álbum: <Text style={styles.text}>{item.albumMusica}</Text></Text>
+                                    </View>
                                 </TouchableOpacity>
+                                <View>
+                                    <TouchableOpacity onPress={() => { deleteMusicas(item.id) }}>
+                                        <Text>Excluir</Text>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
-                        </View>
-                    );
-                }}
-            />
-            <TouchableOpacity onPress={() => navigation.navigate("Cadastrar")}>
-                <Text>+</Text>
-            </TouchableOpacity>
-        </View>
-    );    
+                        );
+                    }}
+                />
+                <TouchableOpacity onPress={() => navigation.navigate("Cadastrar")}>
+                    <Text>+</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    background: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    boxInfo: {
+        flex: 1,
+        backgroundColor: '#00BFFF',
+        padding: 25,
+    },
+    text:{
+        fontSize: 15,
+        fontWeight: 'bold',
+    }
+});
